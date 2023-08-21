08aaac387d99882954d69b540d1a445f8b120199
tsukasa-tenma <tenmatsukasa2@gmail.com>
2023-08-21 17:18:51 +0900

    CAP first commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient, UpdateOne
 import json
 import os
+import time
+import json
+import secrets
 
 mongo_uri = os.environ.get("MONGODB_URI");
 db_client = MongoClient(mongo_uri)
@@ -13,6 +16,68 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     users = get_users()
     return render_template('index.html', data=users)
+
+# Basic route for CAP (chart constant tier list)
+@app.route("/cap")
+def index_cap():
+    constants = get_constants()
+    return render_template('cap.html', data=constants)
+
+# Route for editing your personal tier list
+@app.route("/cap/edit")
+def index_cap_edit():
+    # Must have arguments passed in via a link: user id and key
+    args = request.args
+    # Ensure the user and key were passed in
+    if not "user" in args:
+        return redirect(url_for('index_cap'))
+    if not "key" in args:
+        return redirect(url_for('index_cap'))
+    user_id = args.get("user")
+    # Find a matching key of type "open"
+    matching_key = db["keys"].find_one({"user": user_id, "key": args.get("key"), "keytype": "open"})
+    if matching_key is None:
+        return redirect(url_for('index_cap'))
+    # Make sure the key hasn't expired yet
+    if matching_key["expires"] < time.time():
+        return redirect(url_for('index_cap'))
+    # User has been authenticated. Make a new key and send it to the user
+    new_key = str(secrets.randbelow(10 ** 16))
+    expires_at = time.time() + 36000 # 10 hours
+    # Get the user's ratings
+    user_ratings = get_user_ratings(user_id)
+    # Clear existing keys for that user
+    db["keys"].delete_many({"user": str(user_id)})
+    # Make them a new key
+    db["keys"].insert_one({"user": str(user_id), "key": new_key, "keytype": "edit", "expires": expires_at})
+    # Send them the key and their user ratings
+    user_data = {"user": str(user_id), "key": new_key, "ratings": get_user_ratings(user_id), "edit": True}
+    user_data_str = json.dumps(user_data)
+    return render_template('cap.html', data=user_data_str)
+
+# Route for submitting tier list edits
+@app.route("/cap/submit", methods=["POST"])
+def cap_submit():
+    # Submitted data
+    data = request.json
+    if not "user" in data:
+        return edit_failed()
+    if not "key" in data:
+        return edit_failed()
+    user_id = data["user"]
+    key = data["key"]
+    # Find a matching key of type "edit"
+    matching_key = db["keys"].find_one({"user": user_id, "key": key, "keytype": "edit"})
+    if matching_key is None:
+        return edit_failed()
+    # Make sure the key hasn't expired yet
+    if matching_key["expires"] < time.time():
+        return edit_failed()
+    # Post chart rating edits
+    operations = [UpdateOne({"user": user_id, "song": edit["song"], "diff": edit["diff"]}, {"$set": {"user": user_id, "song": edit["song"], "diff": edit["diff"], "cc": edit["cc"]}}, upsert=True) for edit in data["edits"]]
+    result = db["ratings"].bulk_write(operations)
+    db["ratings"].delete_many({"cc": None})
+    return json.dumps({"success": True, "message": "Edit succeeded"})
 
 def get_all(collection):
     arr = []
@@ -28,6 +93,26 @@ def get_all(collection):
 def get_users():
     users = get_all("users")
     return json.dumps(users)
+
+# Get computed chart constants
+def get_constants():
+    constants = get_all("constants")
+    return json.dumps({"constants": constants, "edit": False})
+
+# Get user ratings
+def get_user_ratings(user):
+    arr = []
+    user_ratings = db["ratings"].find({"user": user})
+    for item in user_ratings:
+        to_remove = ["_id", "id", "user"]
+        for key in to_remove:
+            if key in item:
+                item.pop(key)
+        arr.append(item)
+    return arr
+
+def edit_failed():
+    return json.dumps({"success": False, "message": "Edit failed"})
 
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ static/cap/script.js
@@ -0,0 +1,232 @@
+var musicMasterData;
+const DIFF_NAMES = ['easy', 'normal', 'hard', 'expert', 'master'];
+const DIFF_COLORS = [[134, 218, 69], [95, 184, 233], [243, 174, 60], [220, 82, 104], [172, 62, 230]];
+var showingLevel = 26;
+var editing = false;
+var edits = [];
+var drake;
+var userData;
+
+$(async function () {
+    $("#editBar").hide();
+    await getFiles();
+    makeHeader();
+    initDrag();
+    $(".editBarSave").click(function() {
+        submitRatings();
+    })
+    clearQueryString();
+    let data = loadData();
+    userData = data;
+    showSongList(26, data.edit);
+    updateEdits();
+});
+
+function makeHeader() {
+    $("#header").empty();
+    for (let i = 26; i <= 32; i++) {
+        let div = $(`<div class="diffNum" diff-id="${i}">${i}</div>`);
+        div.click(function () {
+            showSongList(i, editing);
+        });
+        $("#header").append(div);
+    }
+}
+
+async function getFiles() {
+    let musicData = await $.ajax("https://raw.githubusercontent.com/Sekai-World/sekai-master-db-diff/main/musics.json");
+    musicData = JSON.parse(musicData);
+    let diffData = await $.ajax("https://raw.githubusercontent.com/Sekai-World/sekai-master-db-diff/main/musicDifficulties.json");
+    diffData = JSON.parse(diffData);
+    let musics = [];
+    for (let i = 0; i < musicData.length; i++) {
+        musics.push({
+            id: musicData[i].id,
+            title: musicData[i].title,
+            diff: [0, 0, 0, 0, 0],
+            cc: [0, 0, 0, 0, 0]
+        });
+    }
+    for (let i = 0; i < diffData.length; i++) {
+        let dd = diffData[i];
+        let id = dd.musicId;
+        let song = musics.find(function (a) {
+            return a.id == id;
+        });
+        let diffId = DIFF_NAMES.findIndex(function (a) {
+            return a == dd.musicDifficulty;
+        });
+        song.diff[diffId] = dd.playLevel;
+        song.cc[diffId] = dd.playLevel;
+        song.decided = [false, false, false, false, false];
+    }
+    masterMusicData = musics;
+}
+
+async function showSongList(level, editMode) {
+    showingLevel = level;
+    editing = editMode;
+    $(".diffNum").removeClass("diffSelected");
+    $(`.diffNum[diff-id="${level}"]`).addClass("diffSelected");
+    $("#songList").empty();
+    $("#editBarSongs").empty();
+    let ccDivs = [];
+    for (let i = 0; i < 11; i++) {
+        let div = $(`<div class="row"></div>`);
+        let num = level;
+        if (i < 10) {
+            num = level + "." + (9 - i);
+        } else {
+            num = level + ".?";
+        }
+        div.append(`<div class="crNumber wide${i == 10 ? " gray" : ""}">${num}</div>`);
+        div.append(`<div class="ccSongs${editMode ? " canDrag" : ""}" cc-id="${9-i}"></div>`);
+        $("#songList").append(div);
+        if (i == 9 && !editMode) {
+            $("#songList").append(`<div class="tableGap"></div>`);
+        }
+        ccDivs.push(div);
+    }
+    if (editMode) {
+        ccDivs[10].hide();
+    }
+    for (let i = 0; i < masterMusicData.length; i++) {
+        let song = masterMusicData[i];
+        for (let j = 0; j < 5; j++) {
+            if (song.diff[j] == level) {
+                let div = $(`<div class="ccSong" song-id="${song.id}" diff-id="${j}"></div>`);
+                let dc = DIFF_COLORS[j];
+                let color = `rgb(${dc[0]},${dc[1]},${dc[2]})`;
+                div.css({
+                    "background-image": getJacketImage(song.id),
+                    "border": `2px solid ${color}`,
+                    "background-color": color
+                });
+                if (editMode) {
+                    let existingData = userData.ratings.find(function(a) {
+                        return a.song == song.id && a.diff == j;
+                    });
+                    let existingEdit = edits.find(function (a) {
+                        return a.song == song.id && a.diff == j;
+                    });
+                    if (existingEdit && existingEdit.cc > -1) {
+                        ccDivs[9 - existingEdit.cc].find(".ccSongs").append(div);
+                    }
+                    else if (existingData && existingData.cc > -1) {
+                        ccDivs[9 - existingData.cc].find(".ccSongs").append(div);
+                    }
+                    else {
+                         $("#editBarSongs").append(div);
+                    }
+                } else {
+                    let ccDiv = 10;
+                    if (song.decided[j]) {
+                        ccDiv = 9 - Math.round((song.cc[j] - song.diff[j]) * 10);
+                    }
+                    ccDivs[ccDiv].find(".ccSongs").append(div);
+                }
+            }
+        }
+    }
+    if (editMode) {
+        $("#editBar").show();
+        updateEdits();
+    }
+}
+
+function getJacketImage(id) {
+    let songIdCode = id + "";
+    while (songIdCode.length < 3) {
+        songIdCode = "0" + songIdCode;
+    }
+    return `url(https://storage.sekai.best/sekai-assets/music/jacket/jacket_s_${songIdCode}_rip/jacket_s_${songIdCode}.png)`;
+}
+
+function initDrag() {
+    drake = dragula({
+        isContainer: function (el) {
+            return el.classList.contains('canDrag');;
+        },
+        accepts: function (el) {
+            return el.classList.contains('ccSong');
+        }
+    });
+    drake.on("drop", function (el, target, source, sibling) {
+        let songId = $(el).attr("song-id");
+        let diffId = $(el).attr("diff-id");
+        let cc = $(target).attr("cc-id");
+        editCC(parseInt(songId), parseInt(diffId), parseInt(cc));
+    });
+}
+
+function editCC(songId, diffId, cc) {
+    if (!editing) {
+        return;
+    }
+    let existingEdit = edits.find(function (a) {
+        return a.song == songId && a.diff == diffId;
+    });
+    if (existingEdit) {
+        existingEdit.cc = cc;
+    } else {
+        edits.push({
+            song: songId,
+            diff: diffId,
+            cc: cc
+        });
+    }
+    updateEdits();
+}
+
+function updateEdits() {
+    $(".editBarCount").text(`${edits.length} edit${edits.length == 1 ? "" : "s"}`);
+    $(".ccSong").removeClass("edited");
+    for (let i = 0; i < edits.length; i++) {
+        $(`.ccSong[song-id="${edits[i].song}"][diff-id="${edits[i].diff}"]`).addClass("edited");
+    }
+}
+
+function clearQueryString() {
+    let baseUrl = window.location.origin + window.location.pathname;
+    window.history.replaceState({}, document.title, baseUrl);
+}
+
+function loadData() {
+    let text = $("#data").text();
+    if (text == "{{data}}") {
+        return {
+            "edit": true
+        };
+    }
+    let data = JSON.parse(text);
+    return data;
+}
+
+function submitRatings() {
+    let ratingData = {
+        "user": userData.user,
+        "key": userData.key,
+        "edits": edits
+    }
+    console.log(ratingData);
+    $.ajax({
+        url: "/cap/submit",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(ratingData),
+        success: function (response) {
+            response = JSON.parse(response);
+            console.log(response);
+            if (response.success) {
+                edits = [];
+                updateEdits();
+            }
+            else {
+                alert(response.message);
+            }  
+        },
+        error: function (error) {
+            console.log(error); // Handle errors here
+        }
+    });
+}
